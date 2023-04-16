44a45555bdb883e0b4c2d5a7e5870231ed1e02d9
Givi3 <123013167+Givi3@users.noreply.github.com>
2023-04-16 21:36:33 +0300

    homework1.16.04

---
+++ script.js
@@ -0,0 +1,92 @@
+// let d = 7;
+// console.log(d)
+// console.log(d**2);
+// console.log(d**3);
+// --------------------
+
+// let num = Number(prompt('Enter the number'));
+// if (num > 0){
+//     console.log(num*2);
+// }    
+// else {
+//   console.log('Enter the positiv integer');
+// }    
+// --------------------------------------------
+
+// let num = Number(prompt('Enter the number'));
+// let num1 = Number(prompt('Enter the second number'));
+// if(num > num1){
+//     console.log(num + num1);
+// }
+// else if(num1 > num){
+//     console.log(num, num1);
+// }
+// else if (num == num1){
+//     console.log('The same numbers');
+
+// }
+// -----------------------------------------
+
+// let num = Number(prompt('Enter the negative nnumber'));
+// if (num < 0){
+//     console.log(num**2);
+// }
+// else{
+//     console.log('Mistake');
+// }
+// -----------------------------------------------
+
+// let student = Number(prompt('Enter the quantity of students'));
+// let chair = Number(prompt('Enter the quantity of chairs'));
+// if(student < chair){
+//     console.log('Enough chairs');
+// }
+// else if(student > chair){
+//     console.log('Chairs not enough');
+// }
+// ------------------------------------------------------
+
+
+// let num1 = []
+
+// for(let i=1; i < 10; i++){
+//     num1.push(i);
+// }
+// console.log('start');
+// console.log(num1);
+// console.log('Finish');
+// -----------------------------
+
+// let num = []
+// for(let i = 10; i < 100; i+=5){
+
+//     console.log(i)
+// }
+// ---------------------------------
+
+// let arr = [1, 50, 10, 2, 15, 12, 25, 34, 45, 55, 5, 7, 65, 70, 72, 110, 100];
+
+// for(let i = 0; i < arr.length; i++){
+    
+//    if (arr[i] >= 15){
+//     console.log(arr[i]);
+//    }
+   
+// }
+// ---------------------------------------------------
+
+// let num = [4, 3, 6, 5, 15, 12, 17, 19, 22, 34, 33, 43, 37, 49]
+
+// for(let i = 0; i < num.length; i++){
+//     if (num[i] % 2 == 1){
+//         console.log(num[i]);
+//     }
+// }
+// ------------------------------------------------------------
+
+let fruits = ['Banana', 'Pear', 'Mango', 'Apple', 'Mandarin', 'Peach', 'Kiwi', 'Grape', 'Plum', 'Grapefruit', 'Coconut']
+
+for(let i = 3; i < fruits.length; i+=3){
+    console.log(fruits[i]);
+    
+}
